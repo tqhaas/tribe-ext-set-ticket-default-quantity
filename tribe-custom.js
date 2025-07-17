e0823b9d00d021a149f60e45b707267399ea2aae
tqhaas <26669131+tqhaas@users.noreply.github.com>
2025-07-17 13:57:36 +0200

    Update tribe-custom.js
    Add support for enabling first available tickets.

--- tribe-custom.js
+++ tribe-custom.js
@@ -1,9 +1,52 @@
 jQuery(document).ready(function($) {
-    var $ticketRows = $('.tribe-tickets__item');
-    if ($ticketRows.length === 1) {
-        var $qtyInput = $ticketRows.find('input[type="number"]');
-        if ($qtyInput.length && $qtyInput.val() === "0") {
-            $qtyInput.val(1).change();
+    var ticketSet = false;
+
+    // Ticket Views V2
+    $('.tribe-tickets__tickets-item').each(function() {
+        var $ticket = $(this);
+        var $qtyInput = $ticket.find('input[type="number"]');
+        var $availableSpan = $ticket.find('.tribe-tickets__tickets-item-extra-available-quantity');
+        var available = parseInt($availableSpan.text().trim(), 10);
+
+        if (
+            !ticketSet &&
+            $qtyInput.length &&
+            !$qtyInput.prop('disabled') &&
+            available > 0 &&
+            (parseInt($qtyInput.val(), 10) === 0 || $qtyInput.val() === "")
+        ) {
+            $qtyInput.val(1).trigger('change');
+            ticketSet = true;
+            return false;
         }
+    });
+
+    // Classic Tickets (WooCommerce, EDD)
+    if (!ticketSet) {
+        $('input.tribe-tickets-quantity, .woocommerce .quantity input.qty, .edd.quantity input.edd-input').each(function() {
+            var $input = $(this);
+            if (
+                !$input.prop('disabled') &&
+                (parseInt($input.val(), 10) === 0 || $input.val() === "")
+            ) {
+                $input.val(1).trigger('change');
+                ticketSet = true;
+                return false;
+            }
+        });
+    }
+
+    // RSVP Tickets
+    if (!ticketSet) {
+        $('.tribe-rsvp input[type="number"]').each(function() {
+            var $input = $(this);
+            if (
+                !$input.prop('disabled') &&
+                (parseInt($input.val(), 10) === 0 || $input.val() === "")
+            ) {
+                $input.val(1).trigger('change');
+                return false;
+            }
+        });
     }
 });

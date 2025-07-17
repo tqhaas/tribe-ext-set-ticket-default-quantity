5773cb52163163a05e9fea00f9a9c0ec2accdf0e
tqhaas <26669131+tqhaas@users.noreply.github.com>
2025-07-17 11:05:46 +0200

    Update tribe-custom.js
    Set default quantity to one only if a single ticket exists. Leave zero otherwise.

--- tribe-custom.js
+++ tribe-custom.js
@@ -1,32 +1,32 @@
-;( function ($) {
+;(function ($) {
     var container = $('.tribe-events-tickets');
     var block_container = $('form#tribe-tickets');
     var v2Container = $('.tribe-tickets__tickets-form');
 
     var Tribe_Ticket_Qty = {
         init: function () {
-            var self = this;
-            if ( container.length ) {
-                $inputs = container.find('input.tribe-tickets-quantity, .woocommerce .quantity input.qty, .edd.quantity input.edd-input');
-                $inputs.val(1);
-                $inputs.trigger('change');
+            var $inputs = $(); // Start with empty jQuery object
+
+            if (container.length) {
+                $inputs = $inputs.add(container.find('input.tribe-tickets-quantity, .woocommerce .quantity input.qty, .edd.quantity input.edd-input'));
             }
 
-            if ( block_container.length ) {
-                $inputs = block_container.find('input.tribe-tickets-quantity');
-                $inputs.val(1);
-                $inputs.trigger('change');
+            if (block_container.length) {
+                $inputs = $inputs.add(block_container.find('input.tribe-tickets-quantity'));
             }
 
-            if ( v2Container.length ) {
-                $( '.tribe-tickets__tickets-item-quantity-add' ).trigger( 'click' );
+            // Only set value if exactly one input is found across all containers
+            if ($inputs.length === 1) {
+                $inputs.val(1).trigger('change');
+            }
+
+            if (v2Container.length) {
+                $('.tribe-tickets__tickets-item-quantity-add').trigger('click');
             }
         },
-
     };
 
-    $( function () {
+    $(function () {
         Tribe_Ticket_Qty.init();
-    } );
-
-} ) ( jQuery );
+    });
+})(jQuery);

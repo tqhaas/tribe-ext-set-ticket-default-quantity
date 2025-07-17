5df140a1541e32d0829592f9503a10f0324f576e
tqhaas <26669131+tqhaas@users.noreply.github.com>
2025-07-17 11:56:53 +0200

    Update tribe-custom.js
    Better Version provided by the BOT of Support @ The Events Calendar <support@theeventscalendar.com> 
    
    Only supports newer layouts.

--- tribe-custom.js
+++ tribe-custom.js
@@ -1,32 +1,9 @@
-;(function ($) {
-    var container = $('.tribe-events-tickets');
-    var block_container = $('form#tribe-tickets');
-    var v2Container = $('.tribe-tickets__tickets-form');
-
-    var Tribe_Ticket_Qty = {
-        init: function () {
-            var $inputs = $();
-
-            if (container.length) {
-                $inputs = $inputs.add(container.find('input.tribe-tickets-quantity, .woocommerce .quantity input.qty, .edd.quantity input.edd-input'));
-            }
-
-            if (block_container.length) {
-                $inputs = $inputs.add(block_container.find('input.tribe-tickets-quantity'));
-            }
-
-            if ($inputs.length === 1) {
-                $inputs.val(1).trigger('change');
-            }
-
-            // Trigger click only if exactly one Ticket V2 container is found
-            if (v2Container.length === 1) {
-                $('.tribe-tickets__tickets-item-quantity-add').trigger('click');
-            }
-        },
-    };
-
-    $(function () {
-        Tribe_Ticket_Qty.init();
-    });
-})(jQuery);
+jQuery(document).ready(function($) {
+    var $ticketRows = $('.tribe-tickets__item');
+    if ($ticketRows.length === 1) {
+        var $qtyInput = $ticketRows.find('input[type="number"]');
+        if ($qtyInput.length && $qtyInput.val() === "0") {
+            $qtyInput.val(1).change();
+        }
+    }
+});
